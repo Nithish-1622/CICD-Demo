3ebb8b052595c1ac85dfe8f1449994bfd81d1356
Nithish S <nithish.s.1622@gmail.com>
2026-05-12 14:28:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CI/CD Demo Successful 🚀"
+    return "CI/CD Demo Successful 🚀 and Saaiprasath weds Pig"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
